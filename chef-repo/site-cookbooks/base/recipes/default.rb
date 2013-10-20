b601cc0b7deb6dac72b992658e8d24018406d54c
keisatou <dummy>
2013-10-20 23:12:17 +0900

    install plenv and Mojolicious

--- chef-repo/site-cookbooks/base/recipes/default.rb
+++ chef-repo/site-cookbooks/base/recipes/default.rb
@@ -2,7 +2,29 @@
 # Cookbook Name:: base
 # Recipe:: default
 #
-# Copyright 2013, YOUR_COMPANY_NAME
-#
-# All rights reserved - Do Not Redistribute
-#
+
+service 'iptables' do
+  action [:disable, :stop]
+end
+
+plenv_install "5.18.1" do
+  user   "vagrant"
+  action :install
+end
+
+plenv_global "5.18.1" do
+  user   "vagrant"
+  action :run
+end
+
+%w[
+  Plack
+  Mojolicious
+].each do |mod|
+  plenv_cpanm mod do
+    user    "vagrant"
+    version "5.18.1"
+#    options "--force"
+    action  :install
+  end
+end
